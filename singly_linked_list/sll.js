f075c3ec537db1c3f5297d0dabc8338226c13b12
preethiviy <preethivirajyambem@gmail.com>
2024-08-28 17:40:13 +0530

    sll shift and unshift

--- singly_linked_list/sll.js
+++ singly_linked_list/sll.js
@@ -49,4 +49,30 @@ class SinglyLinkedList{
         }
         return current;
     }
+
+    shift(){
+        if(!this.head) return undefined;
+
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
 }
