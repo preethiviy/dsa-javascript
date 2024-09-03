62a1f625aff76d0c4f57d222d4e0de92480a73a9
preethiviy <preethivirajyambem@gmail.com>
2024-09-03 11:32:32 +0530

    dll shift unshift

--- doubly_linked_list/dll.js
+++ doubly_linked_list/dll.js
@@ -47,4 +47,38 @@ class DoublyLinkedList{
         this.length--;
         return poppedNode;
     }
+
+    shift(){
+        if(!this.head) return undefined;
+
+        let oldHead = this.head;
+        
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+
+        this.length--;
+
+        return oldHead;
+    }
+
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
 }
