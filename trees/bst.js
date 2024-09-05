d9738a8df8aca1a0ddf459fe30002f64ab167e88
preethiviy <preethivirajyambem@gmail.com>
2024-09-05 23:48:09 +0530

    bfs and dfs(preorder, postorder, inorder)

--- trees/bst.js
+++ trees/bst.js
@@ -39,4 +39,77 @@ class BinarySearchTree {
             }
         }
     }
+
+    find(value){
+        if(!this.root) return false;
+
+        let current = this.root;
+        let found = false;
+
+        while(current && !found){
+            if(value < current.value){
+                current = current.left
+            } else if(current > current.right){
+                current = current.right;
+            } else{
+                found = true
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+
+    bfs(){
+        let data = [], queue = [], node = this.root;
+        queue.push(node);
+
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left){
+                queue.push(node.left);
+            }
+            if(node.right){
+                queue.push(node.right);
+            }
+        }
+
+        return data;
+    }
+
+    dfsPreorder() {
+        let data = [];
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsPostorder() {
+        let data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsInorder() {
+        let data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
