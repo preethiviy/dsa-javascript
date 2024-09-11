a4fe84431ec4dd5e17404d0b4d2764002d6fac60
preethiviy <preethivirajyambem@gmail.com>
2024-09-12 01:16:34 +0530

    leetcode problems

---
+++ leetcode/number_of_provinces.js
@@ -0,0 +1,12 @@
+/*
+    There are 'n' cities.
+    Some of them are connected, while some are not.
+    If city 'a' is connected directly with city 'b', and city 'b' is directly connected with city 'c',
+    then city 'a' is connected indirectly with city 'c'.
+    A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+    You are given 'n x n' matrix 'isConnected' where isConnected[i][j] = 1 if ith city and jth city are directly connected,
+    and isConnected[i][j] = 0 otherwise.
+    Return the total number of provinces.
+*/
+
+// Use graph
---
+++ leetcode/valid_parentheses.js
@@ -0,0 +1,46 @@
+/*
+    Given a string s containing just the characters '(', ')', '{', '}', '[', ']', determine if the input string is valid.
+    An input string is valid if:
+        1. Open brackets must be closed by the same type of brackets.
+        2. Open brackets must be closed in the correct order.
+*/
+
+// Use Stack
+
+const isValid = function(s){
+    const stack = [];
+    const parens = "() {} []";
+    let i = 0;
+
+    while(i < s.length){
+        stack.push(s[i]);
+        i++;
+
+        let open = stack[stack.length -2];
+        let close = stack[stack.length -1];
+
+        console.log("open ", open);
+        console.log("close ", close);
+
+        let potentialParens = open + close;
+
+        console.log("potentialParens ", potentialParens);
+        console.log("stack before ", stack);
+
+        if(parens.includes(potentialParens)){
+            stack.pop();
+            stack.pop();
+            console.log("stack after ", stack);
+        }
+    }
+
+    return stack.length === 0;
+}
+
+// function calls
+
+// console.log(isValid("()"));
+// console.log(isValid("()[]{}"));
+// console.log(isValid("(]"));
+// console.log(isValid("([)]"));
+console.log(isValid("{[]}"));
